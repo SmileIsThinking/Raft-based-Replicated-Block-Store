efd42ac3a36f7b3c1d2d1932972cc2659b6152bc
SmileIsThinking <shichunyu233@gmail.com>
2022-04-30 23:11:06 -0500

    add raft persistent store interface

--- server_store.cpp
+++ server_store.cpp
@@ -4,10 +4,18 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <iostream>
+#include <fstream>
 
 int fd = -1;
+int log_fd = -1;
+int state_fd = -1;
+
+
 int curr_seq = 0;
 pthread_rwlock_t rwlock;
+
+pthread_rwlock_t loglock;
+pthread_rwlock_t statelock;
 
 int ServerStore::init(int node_id) {
     pthread_rwlock_init(&rwlock, NULL);
@@ -19,6 +27,24 @@ int ServerStore::init(int node_id) {
         return -1;
     }
     curr_seq = 0;
+    
+
+    pthread_rwlock_init(&loglock, NULL);
+    filename = LOG + std::to_string(node_id);
+    std::cout << filename << std::endl;
+    log_fd = open(filename.c_str(), O_RDWR | O_CREAT, mode);
+    if (log_fd == -1) {
+        return -1;
+    }
+
+    pthread_rwlock_init(&statelock, NULL);
+    filename = STATE + std::to_string(node_id);
+    std::cout << filename << std::endl;
+    state_fd = open(filename.c_str(), O_RDWR | O_CREAT, mode);
+    if (state_fd == -1) {
+        return -1;
+    }
+
     return 0;
 }
 
@@ -86,5 +112,52 @@ int ServerStore::full_write(std::string& content) {
     int len = content.size();
     pwrite(fd, content.c_str(), len, 0);
     pthread_rwlock_unlock(&rwlock);
+    return 0;
+}
+
+
+/* ===================================== */
+/* Raft States Update */
+/* ===================================== */
+
+int ServerStore::append_log() {
+
+    return 0;
+}
+
+int ServerStore::read_log() {
+
+    return 0;
+}
+
+int ServerStore::update_state(int currentTerm, int votedFor) {
+    int ret = pthread_rwlock_wrlock(&statelock);
+    if (ret != 0) {
+        std::cout << "STATE LOCK ERROR!" << std::endl;
+        return -1;
+    }
+
+    std::string s = std::to_string(currentTerm) + " " + std::to_string(votedFor);
+    // lseek(state_fd, 0, SEEK_SET);
+    
+    int len = s.size();
+    pwrite(fd, s.c_str(), len, 0);
+    pthread_rwlock_unlock(&statelock);
+
+    return 0;
+}
+
+int ServerStore::read_state() {
+    // std::cout << "read(" << addr << ")" << std::endl;
+    // char* buf = new char[BLOCK_SIZE];
+    // int ret = pthread_rwlock_rdlock(&rwlock);
+    // if (ret != 0) {
+    //     std::cout << "LOCK ERROR!" << std::endl;
+    //     return -1;
+    // }
+    // pread(fd, buf, BLOCK_SIZE, addr);
+    // pthread_rwlock_unlock(&rwlock);
+    // value = std::string(buf);
+    // delete[] buf;
     return 0;
 }
--- server_store.h
+++ server_store.h
@@ -2,14 +2,22 @@
 
 #define STORE "BLOCK_STORE"
 #define BLOCK_SIZE 0x1000
-// int fd = -1;
+
+#define LOG "LOG"
+#define STATE "STATE"
 
 namespace ServerStore {
 
-int init(int node_id);
-int read(const int64_t addr, std::string& value);
-int write(const int64_t addr, const std::string& value, int64_t& seq);
-int full_read(std::string& content);
-int full_write(std::string& content);
+    int init(int node_id);
+    int read(const int64_t addr, std::string& value);
+    int write(const int64_t addr, const std::string& value, int64_t& seq);
+    int full_read(std::string& content);
+    int full_write(std::string& content);
 
+    int append_log();
+    int read_log();
+    
+    int update_state(int currentTerm, int votedFor);
+    int read_state();
 }
+
